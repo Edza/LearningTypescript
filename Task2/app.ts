6661a58e2bf33b9b134387254d8ce2cacba638e0
edza <edexdev@gmail.com>
2015-01-16 01:27:00 +0200

    work started on ceaser cipher

---
+++ Task2/app.ts
@@ -0,0 +1,27 @@
+﻿//Hello, coders!An important part of programming is being able to apply your programs,
+//so your challenge for today is to create a calculator application that has use in your life.
+//It might be an interest calculator, or it might be something that you can use in the classroom.
+//For example, if you were in physics class, you might want to make a F = M * A calc.
+//EXTRA CREDIT: make the calculator have multiple functions!Not only should it be able
+// to calculate F = M * A, but also A = F / M, and M = F / A!
+
+
+interface Calculator {
+    add(x: number, y: number): number;
+    multiply(x: number, y: number): number;
+}
+
+class MyCalculator implements Calculator {
+    add(x: number, y: number) {
+        return x + y;
+    }
+    multiply(x: number, y: number) {
+        return x * y;
+    }
+}
+
+window.onload = () => {
+    var calc = new MyCalculator();
+    alert(calc.add(2, 2));
+    alert(calc.multiply(2, 3));
+};
